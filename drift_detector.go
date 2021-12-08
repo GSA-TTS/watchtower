3398243888179ede29d39a1e636b742e422b031c
folksgl <Gfolks14@gmail.com>
2021-12-08 15:11:10 -0500

    Remove config viewer
    Removal of the config viewer should result in a much smaller codebase
    that accomplishes the same result. Simple modifications the the Detector
    struct methods accomplish the same thing as calling the cached
    ConfigViewer values, and should generally result in the same number of
    API calls being made to the CF API.
    
    As an example, the ConfigViewer was previously set to execute every X
    seconds (X=60), and the DriftDetector using that viewer used the same X
    value for its interval, just offset by X/2. So eliminating ConfigViewer
    and just relying on DriftDetector to make calls to CF ListApps calls
    when it needs to is both simpler, and still results in a single call to
    ListApps.

--- drift_detector.go
+++ drift_detector.go
@@ -2,34 +2,40 @@ package main
 
 import (
 	"log"
+	"net/url"
+	"sync"
 	"time"
+
+	"github.com/cloudfoundry-community/go-cfclient"
 )
+
+// DetectionInterval is the default, recommended scrape interval for Watchtower
+const DetectionInterval = time.Minute * time.Duration(5)
 
 // Detector is used to find drift between the deployed Cloud Foundry resources
 // and those in the provided config allow list.
 type Detector struct {
-	viewer *ConfigViewer
+	client *cfclient.Client
 	config ResourceConfig
 }
 
 // NewDetector starts and returns a new default Detector
 func NewDetector() Detector {
-	viewer, err := NewConfigViewer(DefaultScrapeInterval)
-	if err != nil {
-		log.Fatalf("Detector failed to initialize ConfigViewer: %s", err)
-	}
 	resourceConfig := LoadResourceConfig(nil)
-	detector := Detector{viewer, resourceConfig}
+	detector := Detector{NewCFClient(), resourceConfig}
+
+	// Call .Validate() before returning the detector so that exported metrics aren't
+	// evaluated at their zero-values before the .start() goroutine can can .Validate().
+	// This will prevent an external monitoring system from seeing spurious resets to
+	// zero after watchtower restarts.
+	detector.Validate()
 	go detector.start()
 	return detector
 }
 
+// Start the Detector, calling .Validate every DetectionInterval
 func (detector *Detector) start() {
-	interval := detector.viewer.RefreshInterval()
-
-	// Offset the refresh and validation cycles by 1/2 viewer refresh interval
-	detectionCycleOffset := interval / 2
-	time.Sleep(detectionCycleOffset)
+	interval := DetectionInterval
 	ticker := time.NewTicker(interval)
 	log.Printf("Starting Detector with refresh interval: %fs", interval.Seconds())
 	for range ticker.C {
@@ -37,12 +43,28 @@ func (detector *Detector) start() {
 	}
 }
 
+// Validate queries the CF API and validates responses against the Watchtower config.
+// Results of (non-)compliance are exported as prometheus metrics via the /metrics endpoint.
 func (detector *Detector) Validate() {
-	detector.validateApps()
+	// Parallelize calls to validateX using goroutines and a sync.WaitGroup
+	var waitgroup sync.WaitGroup
+
+	waitgroup.Add(1)
+	go detector.validateApps(&waitgroup)
+
+	waitgroup.Wait()
 }
 
-func (detector *Detector) validateApps() {
-	deployedApps := detector.viewer.V3Apps()
+// ValidateApps performs CF App resource validation
+func (detector *Detector) validateApps(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	deployedApps, err := detector.client.ListV3AppsByQuery(url.Values{})
+	if err != nil {
+		log.Printf("ERROR in app refresh: %s. Skipping check.", err)
+		failedAppUpdates.Inc()
+		return
+	}
 	unknownApps := 0
 OUTER:
 	for _, deployedApp := range deployedApps {
