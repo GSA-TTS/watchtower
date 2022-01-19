91366d7b3f6c4a80ec7bbbf9a72b938e068bd68e
folksgl <Gfolks14@gmail.com>
2022-01-19 17:05:37 -0500

    Add top-level "Config" type
    In order to perform lookups on specific entries and answer ".contains"
    type questions about the expected state of the targeted cloudfoundry
    environment, we need map versions of the slices we currently construct
    from the provided YAML. The new Config type satisfies that role and
    serves as a separation layer between the YAML representation of
    Watchtower's config and the interface we use to interact with that
    config.

--- resource_config.go
+++ resource_config.go
@@ -7,21 +7,32 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// WatchtowerConfig is the desired resource configuration to monitor with Watchtower
-type WatchtowerConfig struct {
+// Config is the top-level configuration type for watchtower. The Config type
+// should be the primary method of reading the expected state of a cloudfoundry
+// environment.
+type Config struct {
+	Data   YAMLConfig
+	Apps   map[string]AppEntry
+	Spaces map[string]SpaceEntry
+}
+
+// YAMLConfig is the desired resource configuration to monitor with Watchtower
+type YAMLConfig struct {
 	AppConfig   AppConfig   `yaml:"apps"`
 	SpaceConfig SpaceConfig `yaml:"spaces"`
 }
 
 // AppConfig represents the Watchtower app_config config file section.
 type AppConfig struct {
-	Enabled bool       `yaml:"enabled"`
-	Apps    []AppEntry `yaml:"cf_apps"`
+	Enabled      bool       `yaml:"enabled"`
+	Apps         []AppEntry `yaml:"cf_apps"`
+	optionalApps map[string]AppEntry
 }
 
 // AppEntry represents the Watchtower
 type AppEntry struct {
-	Name string `yaml:"name"`
+	Name     string `yaml:"name"`
+	Optional bool   `yaml:"optional"`
 }
 
 type SpaceConfig struct {
@@ -38,7 +49,7 @@ type SpaceEntry struct {
 // LoadResourceConfig reads config.yaml and parses it into a ResourceConfig. If
 // dataSource is nil, it will attempt to read from `config.yaml` in the current
 // directory.
-func LoadResourceConfig(dataSource []byte) WatchtowerConfig {
+func LoadResourceConfig(dataSource []byte) Config {
 	if dataSource == nil {
 		log.Printf("Reading config from config.yaml...")
 		var err error
@@ -52,10 +63,22 @@ func LoadResourceConfig(dataSource []byte) WatchtowerConfig {
 	expandedString := os.ExpandEnv(string(dataSource))
 	dataSource = []byte(expandedString)
 
-	var watchtowerConfig WatchtowerConfig
-	if err := yaml.Unmarshal(dataSource, &watchtowerConfig); err != nil {
+	var yamlConfig YAMLConfig
+	if err := yaml.Unmarshal(dataSource, &yamlConfig); err != nil {
 		log.Fatalf("Error parsing config file: %s", err)
 	}
 
-	return watchtowerConfig
+	var conf Config
+	conf.Data = yamlConfig
+	conf.Apps = make(map[string]AppEntry)
+	conf.Spaces = make(map[string]SpaceEntry)
+	for _, app := range conf.Data.AppConfig.Apps {
+		conf.Apps[app.Name] = app
+	}
+
+	for _, space := range conf.Data.SpaceConfig.Spaces {
+		conf.Spaces[space.Name] = space
+	}
+
+	return conf
 }
--- resource_config_test.go
+++ resource_config_test.go
@@ -11,6 +11,7 @@ apps:
   cf_apps:
     - name: my-cool-app
     - name: another-app
+      optional: true
 spaces:
   enabled: true
   cf_spaces:
@@ -21,21 +22,32 @@ spaces:
     - name: prod
       allow_ssh: false`
 
-var basicWatchtowerConfig = WatchtowerConfig{
-	AppConfig: AppConfig{
-		Enabled: true,
-		Apps: []AppEntry{
-			{"my-cool-app"},
-			{"another-app"},
+var basicWatchtowerConfig = Config{
+	Data: YAMLConfig{
+		AppConfig: AppConfig{
+			Enabled: true,
+			Apps: []AppEntry{
+				{Name: "my-cool-app", Optional: false},
+				{Name: "another-app", Optional: true},
+			},
+		},
+		SpaceConfig: SpaceConfig{
+			Enabled: true,
+			Spaces: []SpaceEntry{
+				{Name: "dev", AllowSSH: true},
+				{Name: "test", AllowSSH: true},
+				{Name: "prod", AllowSSH: false},
+			},
 		},
 	},
-	SpaceConfig: SpaceConfig{
-		Enabled: true,
-		Spaces: []SpaceEntry{
-			{Name: "dev", AllowSSH: true},
-			{Name: "test", AllowSSH: true},
-			{Name: "prod", AllowSSH: false},
-		},
+	Apps: map[string]AppEntry{
+		"my-cool-app": {Name: "my-cool-app", Optional: false},
+		"another-app": {Name: "another-app", Optional: true},
+	},
+	Spaces: map[string]SpaceEntry{
+		"dev":  {Name: "dev", AllowSSH: true},
+		"test": {Name: "test", AllowSSH: true},
+		"prod": {Name: "prod", AllowSSH: false},
 	},
 }
 
@@ -54,6 +66,7 @@ apps:
   cf_apps:
     - name: ${TEST_APP_1_NAME}
     - name: $TEST_APP_2_NAME
+      optional: true
 spaces:
   enabled: true
   cf_spaces:
