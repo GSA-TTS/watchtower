02132a955bf202a8fa7284e135d3b23e55df53d2
folksgl <Gfolks14@gmail.com>
2022-04-08 12:40:52 -0400

    Fix cognitive complexity 16 of func '(*Detector).validateAppRoutes' is high (> 10) (gocognit)
    Refactor the loops that populate the 'missingRoutes' and 'unknownRoutes'
    into their own methods to simplify the 'validateAppRoutes' method.

--- drift_detector.go
+++ drift_detector.go
@@ -95,18 +95,9 @@ func (detector *Detector) Validate() {
 	waitgroup.Wait()
 }
 
-// ValidateAppRoutes performs CF App Route resource validation
-func (detector *Detector) validateAppRoutes(wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	var cache = &detector.cache
-
-	if !cache.isValid() {
-		log.Println("Invalid cache detected. Skipping routes check.")
-		failedRouteChecks.Inc()
-		return
-	}
-
+// getMissingRoutes will return a slice of strings representing missing routes in the form
+// <app_name>:<app_hostname>.<app_domain>
+func (detector *Detector) getMissingRoutes() []string {
 	var missingRoutes []string
 	for _, app := range detector.config.Apps {
 		for _, route := range app.Routes {
@@ -117,9 +108,15 @@ func (detector *Detector) validateAppRoutes(wg *sync.WaitGroup) {
 		}
 	}
 
+	return missingRoutes
+}
+
+// getUnknownRoutes will return a slice of strings representing unknown routes in the form
+// <app_name>:<app_hostname>.<app_domain>
+func (detector *Detector) getUnknownRoutes() []string {
 	var unknownRoutes []string
-	for _, mapping := range cache.RouteMappings.routeMappings {
-		app, route, domainName, err := cache.getMappingResources(mapping.Guid)
+	for _, mapping := range detector.cache.RouteMappings.routeMappings {
+		app, route, domainName, err := detector.cache.getMappingResources(mapping.Guid)
 		if err != nil {
 			continue
 		}
@@ -136,6 +133,24 @@ func (detector *Detector) validateAppRoutes(wg *sync.WaitGroup) {
 			unknownRoutes = append(unknownRoutes, app.Name+":"+routeURL)
 		}
 	}
+
+	return unknownRoutes
+}
+
+// ValidateAppRoutes performs CF App Route resource validation
+func (detector *Detector) validateAppRoutes(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	var cache = &detector.cache
+
+	if !cache.isValid() {
+		log.Println("Invalid cache detected. Skipping routes check.")
+		failedRouteChecks.Inc()
+		return
+	}
+
+	missingRoutes := detector.getMissingRoutes()
+	unknownRoutes := detector.getUnknownRoutes()
 
 	if len(unknownRoutes) != 0 {
 		sort.Strings(unknownRoutes)
