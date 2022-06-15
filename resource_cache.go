e9b882af53abd0f7801ee33b427f723d73e0cea1
folksgl <Gfolks14@gmail.com>
2022-06-15 18:52:12 -0400

    Add sshMap to AppCache using v2 CAPI
    This commit adds an sshMap to the AppCache struct to enable Watchtower
    to monitor ssh access on a per-app basis. To do this, I opted to use the
    aging v2 cloud controller API. The reason behind this is that the new v3
    app structs do not include the "EnableSSH" field. In the v3 api, ssh is
    treated as a "feature" and requires sending a request to the CAPI
    /v3/GUID/enable_ssh endpoint for each app. I tested a sequential
    implementation of this and the code was complex. Parallelizing with
    goroutines would have complicated it even more.
    
    SSH information is not available with the go-cfclient, but if the v2 API
    is ever deprecated, this section can be rewritten using the v3 API.

--- resource_cache.go
+++ resource_cache.go
@@ -179,6 +179,7 @@ type AppCache struct {
 	apps    []cfclient.V3App
 	guidMap map[string]cfclient.V3App
 	nameMap map[string]cfclient.V3App
+	sshMap  map[string]bool
 	logger  *zap.SugaredLogger
 }
 
@@ -193,18 +194,30 @@ func (cache *AppCache) refresh(wg *sync.WaitGroup) {
 		return
 	}
 
+	v2ResourceList, err := client.ListApps()
+	if err != nil {
+		cache.Valid = false
+		cache.logger.Infow("failed refreshing app ssh info", "error", err)
+		return
+	}
+
 	// Convert the app data to a map so that lookups can be performed without iterating over the data every time
 	guidMap := make(map[string]cfclient.V3App)
 	nameMap := make(map[string]cfclient.V3App)
+	sshMap := make(map[string]bool)
 
 	for _, elem := range resourceList {
 		nameMap[elem.Name] = elem
 		guidMap[elem.GUID] = elem
 	}
 
+	for _, elem := range v2ResourceList {
+		sshMap[elem.Name] = elem.EnableSSH
+	}
 	cache.apps = resourceList
 	cache.guidMap = guidMap
 	cache.nameMap = nameMap
+	cache.sshMap = sshMap
 	cache.Valid = true
 }
 
